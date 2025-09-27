87463cbda3df43d7398bd1b2c44ffa2f8d3afe7c
bighi <contato@leonardobighi.com>
2025-09-27 16:46:49 -0300

    Add API library with search function

---
+++ src/lib/sw_api.ts
@@ -0,0 +1,35 @@
+type SearchType = "people" | "movies";
+
+type SearchResult = {
+  uid: string;
+  name: string;
+  url: string;
+};
+
+type SearchResults = SearchResult[];
+
+export type { SearchResult, SearchResults };
+
+const Api = {
+  search: async (searchType: SearchType, query: string): Promise<SearchResults> => {
+    try {
+      const endpoint = searchType === "people" ? "https://swapi.tech/api/people/" : "https://swapi.tech/api/films/";
+      const response = await fetch(`${endpoint}?search=${encodeURIComponent(query)}`);
+      console.log("Fetching from:", `${endpoint}?search=${encodeURIComponent(query)}`);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data.results as SearchResults;
+
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      throw error;
+    }
+  },
+};
+
+export default Api;
+export type { SearchType };
